zamronypj/freepascal

commit 29bf4632db67c9d5b37196e4e6d8371fe470ac67

* support passing [nil] to an array of class/objcclass/javaclass/intf/...     in {$mode objfpc} (mantis #19452)
git-svn-id: http://svn.freepascal.org/svn/fpc/trunk@28862 3ad0048d-3df7-0310-abae-a5850022a9f2

compiler/htypechk.pas
@@ -2583,7 +2583,8 @@ implementation
         def_to   : tdef;
         currpt,
         pt       : tcallparanode;
-        eq       : tequaltype;
+        eq,
+        mineq    : tequaltype;
         convtype : tconverttype;
         pdtemp,
         pdoper   : tprocdef;
@@ -2763,6 +2764,30 @@ implementation
                    eq:=compare_defs_ext(n.resultdef,def_to,n.nodetype,convtype,pdoper,cdoptions);
                    n.free;
                  end
+              else if (def_to.typ=arraydef) and
+                      is_class_or_interface_or_dispinterface_or_objc_or_java(tarraydef(def_to).elementdef) and
+                      is_array_constructor(currpt.left.resultdef) and
+                      assigned(tarrayconstructornode(currpt.left).left) then
+                begin
+                  { ensure that [nil] can be converted to "array of tobject",
+                    because if we just try to convert "array of pointer" to
+                    "array of tobject", we get type conversion errors in
+                    non-Delphi modes }
+                  n:=currpt.left;
+                  mineq:=te_exact;
+                  repeat
+                    if tarrayconstructornode(n).left.nodetype=arrayconstructorrangen then
+                      eq:=te_incompatible
+                    else
+                      eq:=compare_defs_ext(tarrayconstructornode(n).left.resultdef,tarraydef(def_to).elementdef,tarrayconstructornode(n).left.nodetype,convtype,pdoper,cdoptions);
+                    if eq=te_incompatible then
+                      break;
+                    if eq<mineq then
+                      mineq:=eq;
+                    n:=tarrayconstructornode(n).right;
+                  until not assigned(n);
+                  eq:=mineq;
+                end
               else
               { generic type comparision }
                begin

tests/webtbs/tw19452.pp
@@ -1,0 +1,17 @@
+{ %norun }
+
+{$mode objfpc}
+type
+  TMyObject = class
+  public
+    constructor Create(ar: array of TMyObject);
+  end;
+
+constructor TMyObject.Create(ar: array of TMyObject);
+begin
+end;
+
+begin
+  TMyObject.Create([nil]);
+end.
+

commit 1acd61c35dadca730634b380f53509fcbc195587

* fix for issue introduced in r28862: the compiler considered also     dynamic arrays and fixed-length static arrays when looking for     overloads of array constructors, while these are not valid in     such cases (and it also gave an error afterwards when trying to     actually use them). This caused a lot of spurious "can't select     which overloaded routine to call" errors when using many JVM     routines
git-svn-id: http://svn.freepascal.org/svn/fpc/trunk@29393 3ad0048d-3df7-0310-abae-a5850022a9f2

compiler/htypechk.pas
@@ -2780,7 +2780,7 @@ implementation
                    eq:=compare_defs_ext(n.resultdef,def_to,n.nodetype,convtype,pdoper,cdoptions);
                    n.free;
                  end
-              else if (def_to.typ=arraydef) and
+              else if is_open_array(def_to) and
                       is_class_or_interface_or_dispinterface_or_objc_or_java(tarraydef(def_to).elementdef) and
                       is_array_constructor(currpt.left.resultdef) and
                       assigned(tarrayconstructornode(currpt.left).left) then

tests/webtbs/tw19452a.pp
@@ -1,0 +1,24 @@
+{ %norun }
+
+{$mode objfpc}
+type
+  TMyObject = class;
+  TArr = array of TMyObject;
+  TMyObject = class
+  public
+    constructor Create(ar: array of TMyObject); overload;
+    constructor Create(ar: TArr); overload;
+  end;
+
+constructor TMyObject.Create(ar: array of TMyObject);
+begin
+end;
+
+constructor TMyObject.Create(ar: Tarr);
+begin
+end;
+
+begin
+  TMyObject.Create([nil]);
+end.
+